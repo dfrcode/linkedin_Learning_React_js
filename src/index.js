cc595e71ff8e86f245317730ed5583b4f25c6ff2
Denis Frolov <denisfrolov.coder@gmail.com>
2021-04-24 19:42:20 +0300

    using useReducer

--- src/index.js
+++ src/index.js
@@ -1,32 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function GitHubUser({ login }) {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${login}`)
-      .then((res) => res.json())
-      .then(setData)
-      .catch(console.error);
-  }, []);
-  if (data) {
-    return (
-      <div>
-        <h1>{data.login}</h1>
-        <img src={data.avatar_url} style={{width: "100px", borderRadius: "50%"}}/>
-      </div>
-    );
-  }
-  return null;
+function Checkbox() {
+  const [checked, toogle] = useReducer((checked) => !checked, false);
+
+  return (
+    <>
+      <input type="checkbox" value={checked} onChange={toogle}/>
+      {checked ? "checked" : "not checked"}
+    </>
+  );
 }
 
 function App() {
   return (
     <>
-      <GitHubUser login="dfr8938" />
-      <GitHubUser login="dfrcode" />
-      <GitHubUser login="shankshel" />
+      <Checkbox />
     </>
   );
 }
