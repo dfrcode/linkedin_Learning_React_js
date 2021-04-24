87c2322084bae6f7430bb156139b61b98904c3d5
Denis Frolov <denisfrolov.coder@gmail.com>
2021-04-24 19:30:10 +0300

    fetching data with useEffect

--- src/index.js
+++ src/index.js
@@ -2,29 +2,31 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+function GitHubUser({ login }) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((res) => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, []);
+  if (data) {
+    return (
+      <div>
+        <h1>{data.login}</h1>
+        <img src={data.avatar_url} style={{width: "100px", borderRadius: "50%"}}/>
+      </div>
+    );
+  }
+  return null;
+}
+
 function App() {
-  const [val, setVal] = useState("");
-  const [val2, setVal2] = useState("");
-
-  useEffect(() => {
-    console.log(`Field1: ${val}`, [val]);
-  });
-
-  useEffect(() => {
-    console.log(`Field2: ${val2}`, [val, val2]);
-  });
-
   return (
     <>
-      <label>
-        Favorite Phrase:
-        <input value={val} onChange={(e) => setVal(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Second Favorite Phrase:
-        <input value={val2} onChange={(e) => setVal2(e.target.value)} />
-      </label>
+      <GitHubUser login="dfr8938" />
+      <GitHubUser login="dfrcode" />
+      <GitHubUser login="shankshel" />
     </>
   );
 }
