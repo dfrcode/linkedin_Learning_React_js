e1c8ab01652813dc5ba130cd0d78b9639af69f0d
Denis Frolov <denisfrolov.coder@gmail.com>
2021-04-24 15:22:51 +0300

    using multiply setState

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 function App() {
-  const [status, setStatus] = useState("Open")
+  const [year, setYear] = useState(2050);
+  const [manager, setManager] = useState("Alex");
+  const [status, setStatus] = useState("Open");
   return (
     <>
+      <h1>Year: {year}</h1>
+      <button onClick={() => setYear(year + 1)}>New Year!</button>
+      <h1>Manager: {manager}</h1>
+      <button onClick={() => setManager("Kate")}>New Manager</button>
       <h1>Status: {status}</h1>
       <button onClick={() => setStatus("Open")}>Open</button>
       <button onClick={() => setStatus("Back in 5")}>Break</button>
