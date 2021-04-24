e64f1e91f9c483b93e62de0c04660390b75da205
Denis Frolov <denisfrolov.coder@gmail.com>
2021-04-24 14:24:05 +0300

    add react fragments

--- src/index.js
+++ src/index.js
@@ -2,28 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Lake({ name }) {
+function Lake() {
+  return <h1>Lake!</h1>;
+}
+
+function SkiResort() {
+  return <h1>SkiResort!</h1>;
+}
+
+function App() {
   return (
-    <div>
-      <h1>Visit {name}!</h1>
-    </div>
+    <>
+      <Lake />
+      <SkiResort />
+    </>
   );
 }
 
-function SkiResort({ name }) {
-  return (
-    <div>
-      <h1>Visit {name}!</h1>
-    </div>
-  );
-}
-
-function App({ season }) {
-  if (season === "summer") {
-    return <Lake name="Jenny Lake" />;
-  } else if (season === "winter") {
-    return <SkiResort name="JHMR" />;
-  }
-}
-
-ReactDOM.render(<App season="winter" />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
