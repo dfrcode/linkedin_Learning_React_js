cd702c422688766b329733ca758f6adc2c585242
Denis Frolov <denisfrolov.coder@gmail.com>
2021-04-24 10:56:47 +0300

    adding list items as in a simple html

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  React.createElement(
-    "ul",
-    { style: { color: "red" } },
-    React.createElement("li", null, "Hot Dogs"),
-    React.createElement("li", null, "Hamburgers"),
-    React.createElement("li", null, "Pizza")
-  ),
+  <ul>
+    <li>Hot Dogs</li>
+    <li>Hamburger</li>
+    <li>Pizza</li>
+    <li>Sushi</li>
+  </ul>,
   document.getElementById("root")
 );
